867845bffb917ae974a601f6c4dc8ecfd459d8f8
Harel Feldman <harel@neokarm.com>
2021-06-20 18:27:48 +0300

    Add terraform wordpress-3tier-app example

---
+++ Terraform/wordpress-3tier-app/variables.tf
@@ -0,0 +1,30 @@
+variable "secret_key" {
+}
+
+variable "access_key" {
+}
+variable "credentials_file" {
+  default = "~/.aws/credentials"
+}
+
+variable "zCompute_ip" {
+}
+
+variable "web_number" {
+}
+
+variable "web_ami" {
+}
+
+variable "web_instance_type" {
+}
+
+variable "public_keypair_path" {
+}
+
+variable "db_password" {
+}
+
+variable "db_user" {
+}
+
