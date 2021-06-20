f3ba858e6ce8d90368bc455deba5ee099a478639
Harel Feldman <harel@neokarm.com>
2021-06-20 18:23:33 +0300

    Add terraform  expand-lvm-cloudinit-ebs example

---
+++ Terraform/ec2-expand-lvm-cloudinit-ebs/main.tf
@@ -0,0 +1,62 @@
+# ---------------------------------------------------------------------------------------------------------------------
+#     This module creates the following resources:
+#          
+#          * 1 Instance
+#          * Key pair
+#          * Eip
+#           
+#  
+#     This example was tested on versions:
+#     - zCompute version 5.5.3
+#     - terraform 0.12.27 & 0.13
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+# Create instances, and name them according to count
+
+# Defining cloud config template file 
+
+data "template_file" "ebsdeploy" {
+  template = file("./cloudconfig.cfg")
+}
+
+data "template_cloudinit_config" "ebsdeploy_config" {
+  gzip          = false
+  base64_encode = false
+
+  part {
+    filename     = "cloudconfig.cfg"
+    content_type = "text/cloud-config"
+    content      = data.template_file.ebsdeploy.rendered
+  }
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ssh_key"
+  public_key = file(var.public_keypair_path)
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami      = var.instance_image
+  key_name = aws_key_pair.ssh_key.key_name
+  tags = {
+    Name = "instance_expanded"
+  }
+
+  instance_type = var.instance_type
+
+
+  user_data = data.template_cloudinit_config.ebsdeploy_config.rendered
+
+  root_block_device {
+    # Enter larger volume size here in GB, must be larger than images base size
+    volume_size = 200
+  }
+}
+resource "aws_eip" "EIP" {
+  instance   = aws_instance.ec2_instance.id
+  vpc        = true
+  depends_on = [aws_instance.ec2_instance]
+}
+
+
---
+++ Terraform/ec2-expand-lvm-cloudinit-ebs/terraform.tfvars
@@ -0,0 +1,18 @@
+# .tfvars Sample File
+
+
+# Region Credentials
+zCompute_ip = "<zCompute_ip>"
+access_key  = "<access_key>"
+secret_key  = "<secret_key>"
+# credentials_file = "<path_to_creds_file>"
+
+# Recommended use of Xenial's latest cloud image
+# located here: https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img 
+
+instance_image      = "<instance_ami>"
+public_keypair_path = "<path_to_public_key>"
+
+# optional
+# instance_type = "<instance-type>"
+
